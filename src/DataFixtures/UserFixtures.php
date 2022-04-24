ed790a150630ef0540d9001cbc88c749db33a86f
J7 <J7DevWeb@gmail.com>
2022-04-24 22:52:41 +0200

    upload avatar service and user profil

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -6,21 +6,27 @@ use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class UserFixtures extends Fixture
 {
 
     private $encoder;
+    private $slugger;
 
-    public function __construct(UserPasswordHasherInterface $encoder)
+    public function __construct(UserPasswordHasherInterface $encoder, SluggerInterface $slugger)
     {
         $this->encoder = $encoder;
+        $this->slugger = $slugger;
     }
 
     public function load(ObjectManager $manager): void
     {
         $admin = new User();
         $admin->setEmail('admin@admin.fr')
+        ->setName('Jacky l\'admin')
+        ->setSlug($this->slugger->slug($admin->getName().'-'.rand(100, 999))->lower())
+        ->setAvatar('default.png')
         ->setRoles(['ROLE_ADMIN'])
         ->setPassword($this->encoder->hashPassword($admin, 'adminadmin'))
         ;
@@ -28,6 +34,9 @@ class UserFixtures extends Fixture
 
         $user = new User();
         $user->setEmail('user@user.fr')
+        ->setName('Jacky')
+        ->setAvatar('default.png')
+        ->setSlug($this->slugger->slug($user->getName().'-'.rand(100, 999))->lower())
         ->setRoles(['ROLE_USER'])
         ->setPassword($this->encoder->hashPassword($user, 'useruser'))
         ;
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,9 +6,13 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Image;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -19,6 +23,16 @@ class RegistrationFormType extends AbstractType
     {
         $builder
             ->add('email', EmailType::class)
+            ->add('name', TextType::class)
+            ->add('bio', TextareaType::class)
+            ->add('avatar', FileType::class, [
+                'required' => false,
+                'constraints' => [
+                    new Image([
+                        'maxSize' => '1024k'
+                    ])
+                    ],
+            ])
             ->add('plainPassword', PasswordType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
