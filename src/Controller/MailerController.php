f195f5bd63333caa4a0169fefcaeac5c35daa0be
J7 <J7DevWeb@gmail.com>
2022-04-27 09:05:42 +0200

    send simple email with mailer

---
+++ src/Controller/MailerController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class MailerController extends AbstractController
+{
+    /**
+     * @Route("/mailer", name="app_mailer")
+     */
+    public function mailer(MailerInterface $mailer): Response
+    {
+
+        $email = (new Email())
+            ->from('hello@example.com')
+            ->to('you@example.com')
+            ->subject('Time for Symfony Mailer!')
+            ->text('Sending emails is fun again!')
+            ->html('<p>See Twig integration for better HTML integration!</p>');
+
+        $mailer->send($email);
+
+        return $this->redirectToRoute('homepage');
+    }
+}
